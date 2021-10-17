09e452d65ebe334cedfbe730f5c7dc7de06bbc2f
Stephen Allard <stephencallard@gmail.com>
2021-10-17 11:21:20 -0500

    clicking button adds mark to gameBoard array

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
 gameBoard = (function(){
-  var boardArr = [];
+  var boardArr = ["","","","","","","","",""];
+  var buttons = document.querySelectorAll("button");
+  var activeMark = 'x';
+  
+  //add event listeners
+  for(let i=0; i<buttons.length; i++){
+    buttons[i].addEventListener('click', () => {
+      _addMarkToBoardArr(activeMark, buttons[i].getAttribute("id"));
+    });
+  }
+  
+  function _addMarkToBoardArr(mark, coordinate){
+    boardArr[coordinate] = mark;
+    console.log(boardArr);
+    _render();
+  }
+  
+  function _render(){
+    for(let i=0; i<buttons.length; i++){
+      buttons[i].innerText = boardArr[i];
+    }
+  }
+  
 })();
