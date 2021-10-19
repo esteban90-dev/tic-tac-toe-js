42674acbeb2a26e4219142288cc267988c74556c
Stephen Allard <stephencallard@gmail.com>
2021-10-18 21:43:10 -0500

    move buttons & event listeners to the game module

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,48 @@
 gameBoard = (function(){
   var boardArr = ["","","","","","","","",""];
+  
+  function addMark(mark, coordinate){
+    boardArr[coordinate] = mark;
+  }
+
+  function getMark(coordinate){
+    return boardArr[coordinate];
+  }
+  
+  function hasWinner(){
+    //horizontal winner - 0,1,2
+    if(boardArr[0] && boardArr[1] && boardArr[2]){
+      if((boardArr[0] === boardArr[1]) && (boardArr[0] === boardArr[2])){
+        return boardArr[0];
+      }
+    }
+    //vertical winner
+    //diagonal winner
+  }
+  
+  return { addMark, getMark };
+})();
+
+game = (function(){
   var buttons = document.querySelectorAll("button");
   var activeMark = 'x';
-  
+  var board = gameBoard;
+
   //add event listeners
   for(let i=0; i<buttons.length; i++){
-    buttons[i].addEventListener('click', () => {
-      _addMarkToBoardArr(activeMark, buttons[i].getAttribute("id"));
+    buttons[i].addEventListener('click', function(){
+      _handleMark(i);
     });
   }
-  
-  function _addMarkToBoardArr(mark, coordinate){
-    boardArr[coordinate] = mark;
-    console.log(boardArr);
+
+  function _handleMark(coordinate){
+    board.addMark(activeMark, buttons[coordinate].getAttribute("id"));
     _render();
   }
-  
+
   function _render(){
     for(let i=0; i<buttons.length; i++){
-      buttons[i].innerText = boardArr[i];
+      buttons[i].innerText = board.getMark(i);
     }
   }
-  
-})();
+})(gameBoard);
